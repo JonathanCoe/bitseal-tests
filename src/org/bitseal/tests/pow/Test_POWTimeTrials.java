75293d8f520e30e1f92d00df25bd500c71d8c650
JonathanCoe <jcoe@countermail.com>
2014-10-29 10:45:04 +0100

    Fixed a bug in Test_POWTimeTrials

--- src/org/bitseal/tests/pow/Test_POWTimeTrials.java
+++ src/org/bitseal/tests/pow/Test_POWTimeTrials.java
@@ -6,26 +6,27 @@ import java.util.ArrayList;
 import junit.framework.TestCase;
 
 import org.bitseal.pow.POWProcessor;
+import org.bitseal.util.ByteFormatter;
 
 import android.util.Log;
 
 /**
- * Runs time trials for Bitmessage POW in Bitseal, using the
- * POW formula from Protocol Version 3. 
+ * Runs time trials for Bitmessage proof of work calculations
+ * in Bitseal.
  * 
  * @author Jonathan Coe
- *
  */
 public class Test_POWTimeTrials extends TestCase
 {
-	private static final int TRIALS_TO_RUN = 1;
+	private static final int TRIALS_TO_RUN = 3;
 	
-	private static final int PAYLOAD_LENGTH = 800;
+	private static final int PAYLOAD_LENGTH = 500;
+	private static final long TIME_TO_LIVE = 3600; // In seconds, so currently set to 1 hour
 	
 	private static final long NONCE_TRIALS_PER_BYTE = 1000;
 	private static final long EXTRA_BYTES = 1000;
 	
-	private static final String TAG = "POW_TIME_TRIALS_VERSION_3";
+	private static final String TAG = "POW_TIME_TRIALS";
 	
 	protected void setUp() throws Exception
 	{
@@ -40,18 +41,22 @@ public class Test_POWTimeTrials extends TestCase
 	public void testPOWTimeTrials()
 	{
 		byte[] payload = new byte[PAYLOAD_LENGTH];
-		new SecureRandom().nextBytes(payload);
+		SecureRandom secRand = new SecureRandom();
 		POWProcessor powProc = new POWProcessor();
 		ArrayList<Long> times = new ArrayList<Long>();
 		
-		// Set an expiration time of 1 hour from now
-		long expirationTime = (System.currentTimeMillis() / 1000) + 3600;
+		// Set the expiration time for this payload
+		long currentTime = System.currentTimeMillis() / 1000;
+		long expirationTime = currentTime + TIME_TO_LIVE;
 		
 		for (int i = 0; i < TRIALS_TO_RUN; i++)
 		{
  			long startTime = 0;
  			long endTime = 0;
  			long timeTaken = 0;
+ 			
+ 			secRand.nextBytes(payload);
+ 			Log.i(TAG, "About to do POW calculations for the following payload: \n" + ByteFormatter.byteArrayToHexString(payload));
 			
 			//----------------------------------BEGIN TIMED TEST----------------------------------------------
  			startTime = System.nanoTime();
@@ -75,6 +80,6 @@ public class Test_POWTimeTrials extends TestCase
  			sumOfTimes += l;
  		}
  		long averageTime = sumOfTimes / times.size();
- 		Log.i(TAG, "Average time taken in seconds: " + averageTime);
+ 		Log.d(TAG, "Average time taken in seconds:  " + averageTime);
 	}
 }
